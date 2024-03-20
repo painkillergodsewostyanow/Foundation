a749b90c25f8589c3997968f5c5c98ea2d4df65a
Севостьянов Илья <114825656+painkillergodsewostyanow@users.noreply.github.com>
2024-03-21 02:54:41 +0300

    you are not alone modern talking

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.shortcuts import reverse, redirect
 
 
 def index(request):
     if request.user.is_authenticated:
-        return redirect(reverse('education_app:dashboard'))
+        return HttpResponse('редирект на дашборд')
     return render(request, 'main_app/landing.html')
